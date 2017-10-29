5a3dc45af4ad663da36d9e2c89359e1ce55eaaa3
Tammy Ritterskamp <tritterskamp@gmail.com>
2017-10-28 19:42:20 -0500

    Code cleanup

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import base from "./base"
-import restaurantsList from "./restaurantsList";
 import AddRestaurantForm from "./components/AddRestaurantForm";
 import AddWeeklySpecialForm from "./components/AddWeeklySpecialForm";
 import AllRestaurants from "./components/AllRestaurants";
@@ -20,9 +19,6 @@ class App extends Component {
     };
 
     // Bind methods:
-    this.filterTodaysSpecials = this.filterTodaysSpecials.bind(this);
-    this.formatTodaysSpecials = this.formatTodaysSpecials.bind(this);
-    this.getTodaysSpecials = this.getTodaysSpecials.bind(this);
     this.addRestaurant = this.addRestaurant.bind(this);
     this.addSpecial = this.addSpecial.bind(this);    
     this.handleClick = this.handleClick.bind(this);
@@ -38,49 +34,6 @@ class App extends Component {
       context: this,
       state: "weeklySpecials"
     });
-  }
-
-  filterTodaysSpecials(currentDay, weeklySpecialsList) {
-    // Filter through weeklySpecialsList
-    const todaysSpecialList = [];
-    weeklySpecialsList.map(function(restaurant) {
-      for (let i = 0; i < restaurant.weeklySpecials.length; i++) {
-        // If weekly special contains a specialDay that matches the currentDay push restaurant name, specials details and website into a new array
-        if (restaurant.weeklySpecials[i].specialDay === currentDay) {
-          todaysSpecialList.push({
-            restaurantName: restaurant.restaurantName,
-            specialDay: restaurant.weeklySpecials[i].specialDay,
-            special: restaurant.weeklySpecials[i].special
-          });
-        }
-      }
-    });
-    // Return the new array
-    return todaysSpecialList;
-  }
-
-  formatTodaysSpecials(data) {
-    let html = "";
-    if (data.length > 0) {
-      for (let i = 0; i < data.length; i++) {
-        const $this = data[i];
-        html += `<p>${$this.restaurantName} has ${$this.special} on ${$this.specialDay}`;
-      }
-    } else {
-      html += `<p>Sorry, there are no specials today. Try something new!</p>`;
-    }
-    return html;
-  }
-
-  // User clicks specials button, we should return all restaurants with a special matching the day of the week
-  getTodaysSpecials(e) {
-    const today = this.getCurrentWeekday();
-    const weeklySpecialsList = restaurantsList.filter(function(restaurant) {
-      return restaurant.weeklySpecials;
-    });
-    const todaysSpecials = this.filterTodaysSpecials(today, weeklySpecialsList);
-    const resultsMarkup = this.formatTodaysSpecials(todaysSpecials);
-    this.displayResults(resultsMarkup);
   }
 
   // Add new functions
@@ -118,10 +71,10 @@ class App extends Component {
         <h1>Where should we go for dinner?</h1>
         <div>
           <button className="button--weeklySpecials" name="showTodaysSpecials" onClick={e => this.handleClick(e)}>
-            Somewhere Tried & True
+            Today's Specials
           </button>
           <button className="button--newRestaurants" name="showNewRestaurants" onClick={e => this.handleClick(e)}>
-            Give Me Something New
+            Let's Try Something New
           </button>
           <button className="button--weeklySpecials" name="showAllRestaurants" onClick={e => this.handleClick(e)}>
             Show Me All Restaurants
